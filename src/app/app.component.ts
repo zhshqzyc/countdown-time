1bb442aa82e3f220957c03fd6dbb35381e9de504
Hui Zhao <zhshqzyc@gmail.com>
2024-05-14 09:19:23 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountdownTimeComponent } from "./countdown-time/countdown-time.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CountdownTimeComponent, FormsModule ]
 })
 export class AppComponent {
-  title = 'countdown-time';
+
+  parentTime: string = "2024-05-14T12:00:00"; // Set the time value here
+  changeParentTime(newTime: string) {
+    this.parentTime = newTime;
+  }
 }
---
+++ src/app/countdown-time/countdown-time.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-time',
+  standalone: true,
+  imports: [],
+  templateUrl: './countdown-time.component.html',
+  styleUrl: './countdown-time.component.scss'
+})
+export class CountdownTimeComponent {
+  private countDownTime: number = 0;
+  private timerNumerics = {
+    milliseconds: 1000,
+    minuteInSeconds: 60,
+    minutesInHours: 60,
+    hoursInDay: 24
+  };
+  @Input() set time(value: string) {
+    this.countDownTime = new Date(value).getTime();
+  }
+
+  get days(): string {
+    const { milliseconds, minutesInHours, minuteInSeconds, hoursInDay } = this.timerNumerics;
+    const days = Math.floor(this.getTimeDifference() / (
+      milliseconds * minutesInHours * minuteInSeconds * hoursInDay
+    ));
+
+    return this.formatTime(days);
+  }
+
+  get hours(): string {
+    const { milliseconds, minuteInSeconds, minutesInHours, hoursInDay } = this.timerNumerics;
+    const hours = Math.floor(
+      (this.getTimeDifference() % (hoursInDay * minutesInHours * minuteInSeconds * milliseconds)) /
+      (minutesInHours * minuteInSeconds * milliseconds));
+
+    return this.formatTime(hours);
+  }
+
+  get minutes(): string {
+    const { milliseconds, minuteInSeconds, minutesInHours } = this.timerNumerics;
+    const minutes = Math.floor(
+      this.getTimeDifference() % (minutesInHours * minuteInSeconds * milliseconds) / (minuteInSeconds * milliseconds));
+
+    return this.formatTime(minutes);
+  }
+
+  get seconds(): string {
+    const { milliseconds, minuteInSeconds } = this.timerNumerics;
+    const seconds = Math.floor((this.getTimeDifference() % (minuteInSeconds * milliseconds)) / milliseconds);
+
+    return this.formatTime(seconds);
+  }
+
+  private getTimeDifference(): number {
+    const currentTime = new Date().getTime();
+
+    return this.countDownTime - currentTime;
+  }
+
+  private formatTime(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+}
